1b4db2ee81c4abc6583813e355263760d1e45d11
nanitkatipunan <nanit.katipunan@student.passerellesnumeriques.org>
2019-09-24 16:44:43 +0800

    Fix some errors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var port=process.env.PORT ||3000
+var port = process.env.PORT ||3000
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
 
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log('listening on *:' + port);
 
 }); 
+
